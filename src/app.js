5342bb5cb2a7999494740cc5f29d6b7906e346d4
Tobias Haider <thaider@Tobiass-MacBook-Pro.local>
2020-08-15 20:30:00 +0200

    Added database support

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import {addExpense} from './actions/expenses'
-import {setTextFilter} from './actions/filters'
 import 'react-dates/lib/css/_datepicker.css'
-import getVisibleExpenses from './selectors/expenses'
 import './firebase/firebase'
+import {startSetExpenses} from './actions/expenses'
 
 const store = configureStore()
-
 const jsx = (
     <Provider store = {store}>
         <AppRouter />
     </Provider>
 )
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'))
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
 if(process.env.NODE_ENV === 'test'){
    require('dotenv').config({path: '.env.test'})
 } else if (process.env.NODE_ENV === 'development'){
